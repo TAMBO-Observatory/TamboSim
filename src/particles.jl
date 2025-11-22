f118d4921e78e84371975d9e267e62fae296f121
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2025-11-22 01:36:56 +0100

    New particles

--- src/particles.jl
+++ src/particles.jl
@@ -1,51 +1,50 @@
-struct Particle
-    pdg_mc::Int64
-    energy::Float64
-    position::SVector{3, Float64}
-    direction::Direction
+struct Particle{T <: Real, U, V}
+    pdg_id::Int
+    energy::Quantity{T,V,typeof(u"GeV")}
+    position::Coordinate{T,U}
+    direction::Direction{T,U}
 end
 
-const range_parameters = Dict(
-    13 => (1.76666667e-1 * units.GeV / units.mwe, 2.0916666667e-4 / units.mwe),
-    15 => (1.473684210526e3 * units.GeV / units.mwe, 2.63e-5 / units.mwe),
-    -13 => (1.76666667e-1 * units.GeV / units.mwe, 2.0916666667e-4 / units.mwe),
-    -15 => (1.473684210526e3 * units.GeV / units.mwe, 2.63e-5 / units.mwe)
+const null_particle = Particle(
+    0, 
+    0.0u"GeV",
+    Coordinate([0.0u"m", 0.0u"m",  0.0u"m"], ecefcoordinates),
+    Direction([0.0, 0.0, -1.0], ecefcoordinates)
 )
 
-"""
-    lepton_range(e, pdg_code)
+# This is a bad place for this
+const c = 299_792_458.0 * u"m" / u"s"
 
-Compute the 99.9% column depth for a lepton with energy `e` using parametrization
-from https://doi.org/10.1016/j.cpc.2021.108018.
+particle_parameters = Dict{Int, Tuple}(
+    15 => (1776.86 * u"MeV"/c^2, 2.903e-13 * u"s"),
+    -15 => (1776.86 * u"MeV"/c^2, 2.903e-13 * u"s"),
+)
 
-# Example
-```julia-repl
-julia> lepton_range(1units.PeV, false) / units.mwe
-
-julia> lepton_range(1units.PeV, true) / units.mwe
-
-```
-"""
-function lepton_range(e::Float64, pdg_code::Int)
-    α, β = range_parameters[pdg_code]
-    range = log(1 + e * β / α) / β
-    return range
+function gamma(
+    ke::Quantity{T,U,typeof(u"GeV")},
+    m::Quantity{T,V}
+)::T where {T,U,V}
+    return ke / m / c^2
 end
 
-function lepton_range(p::Particle)
-    return lepton_range(p.energy, p.pdg_mc)
+function particle_range(
+    pdg_id::Int,
+    energy::Quantity{T,U},
+    epsilon::Float64=1e-3
+)::Quantity{T,Unitful.𝐋,typeof(u"m")} where {T,U}
+    m, tau = particle_parameters[pdg_id]
+    return -gamma(energy, m) * c * tau * log(epsilon)
 end
 
-function Base.getindex(v::Vector{Particle}, s::String)
-    return getfield.(v, Symbol(s))
+function particle_range(
+    particle::Particle{T,U,V},
+    epsilon::Float64=1e-3
+)::Quantity{T,U,typeof(u"m")} where {T,U,V}
+    return particle_range(particle_range.pdg_id, epsilon)
 end
 
-function Base.show(io::IO, particle::Particle)
-    s = "Particle("
-    s *= "pdg_mc=$(particle.pdg_mc), "
-    s *= "energy=$(particle.energy / units.GeV) GeV, "
-    s *= "position=$(particle.position / units.m)m, "
-    s *= "direction=$(particle.direction)"
-    s *= ")"
-    print(io, s)
-end
+#const range_parameters = Dict(
+#    13 => (1.76666667e-1 * units.GeV / units.mwe, 2.0916666667e-4 / units.mwe),
+#    15 => (1.473684210526e3 * units.GeV / units.mwe, 2.63e-5 / units.mwe),
+#    -13 => (1.76666667e-1 * units.GeV / units.mwe, 2.0916666667e-4 / units.mwe),
+#    -15 => (1.473684210526e3 * units.GeV / units.mwe, 2.63e-5 / units.mwe)

715842686419fe11d41bc796be5d1a122dda3f66
jlazar17 <jeff.p.lazar@gmail.com>
2025-11-26 10:42:55 -0500

    Add last parameter to SMatrix

--- src/geometry/coordinate_system.jl
+++ src/geometry/coordinate_system.jl
@@ -1,7 +1,7 @@
 # Coordinate system
-struct CoordinateSystem{T<:Real,U}
+struct CoordinateSystem{T<:Real, U}
     origin::SVector{3, Quantity{T,U,typeof(u"m")}}
-    rotation::SMatrix{3, 3, T}
+    rotation::SMatrix{3, 3, T, 9}
 end
 
 function CoordinateSystem(longlat::Tuple{Float64, Float64}, rearth::Quantity)
@@ -14,5 +14,5 @@ Base.eltype(cs::CoordinateSystem) = eltype(cs.rotation)
 
 const ecefcoordinates = CoordinateSystem(
     SVector{3}([0.0*u"m", 0.0*u"m", 0.0*u"m"]),
-    SMatrix{3, 3}([1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0])
+    SMatrix{3, 3, Float64, 9}([1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0])
 )

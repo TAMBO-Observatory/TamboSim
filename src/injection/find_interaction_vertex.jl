7d53c4c484c207113d4745a041a191e7619f4254
jlazar17 <jeff.p.lazar@gmail.com>
2025-11-27 06:47:56 -0500

    Simplify distance finding algorithm

--- src/injection/find_interaction_vertex.jl
+++ src/injection/find_interaction_vertex.jl
@@ -4,70 +4,59 @@ rock_density = 2.6 * u"g"/u"cm"^3
 function find_vertex_distance(
     direction::Direction{T},
     distance::Quantity{T},
-    intersections::Vector{Intersection{T}},
+    intersections::I,
     epsilon::Float64=1e-3
-) where {T<:Real}
+) where {T<:Real, I<:AbstractVector{Intersection{T}}}
 
-    prem_densities = [2.9u"g"/u"cm"^3, rock_density, rock_density]
-    prev_distance = 0.0u"km"
+    densities = compute_density(intersections, direction)
+    distances = [ix2.distance-ix1.distance for (ix1, ix2) in zip(intersections, intersections[2:end])]
+    @assert length(densities)==length(distances)
     column_depth = 0.0u"g/cm^2"
-    seen_rock = false
-    for intersection in intersections
-        density = nothing
-        if typeof(intersection)==TriangleIntersection{T}
-            entering_rock = dot(direction, intersection.normal) < 0
-            #@show entering_rock
-            density = entering_rock ? rock_density : air_density
-            if entering_rock
-                seen_rock = true
-            end
-        elseif typeof(intersection)==SphereIntersection{T}
-            seen_rock = true
-            density = pop!(prem_densities)
-        end
-        if ~seen_rock
-            @show "We didn't see rock!!"
-            continue
-        end
-        section_length = intersection.distance - prev_distance
-        if section_length > distance
-            column_depth += distance * density
-            distance = 0.0u"m"
+    d, cd = 0.0u"m", 0.0u"g/cm^2"
+    for (dist, dens) in zip(distances, densities)
+        if d + dist > distance
+            cd += (distance - d) * dens
             break
         end
-        column_depth += section_length * density
-        distance -= section_length
-        prev_distance = intersection.distance
+        cd += dist * dens
+        d += dist
     end
-    if distance > 0.0u"m"
-        println("Distance budget not used up.")
-        @show direction.point
-    end
-    #if column_deptI#h==0.0u""
 
-    idx = 1
-    prem_densities = [2.9u"g"/u"cm"^3, rock_density, rock_density]
-    target_column_depth = rand(Uniform(0, ustrip(column_depth |> u"g/cm^2"))) * u"g/cm^2"
-    tot_cd = target_column_depth
-    prev_distance = 0.0u"km"
-    tot_distance = 0.0u"km"
-    while target_column_depth > 0.0u"g/cm^2"
-        intersection = intersections[idx]
-        density = nothing
-        if typeof(intersection)==TriangleIntersection{T}
-            density = dot(direction, intersection.normal) < 0 ? rock_density : air_density
-        elseif typeof(intersection)==SphereIntersection{T}
-            density = pop!(prem_densities)
+    target_column_depth = rand(Uniform(0, ustrip(cd |> u"g/cm^2"))) * u"g/cm^2"
+    accrued_cd = 0.0u"g/cm^2"
+    accrued_d = 0u"m"
+    for (dist, dens) in zip(distances, densities)
+        segment_cd = dist * dens
+        if segment_cd + accrued_cd > target_column_depth
+            accrued_d += (target_column_depth - accrued_cd) / dens
+            if dens < 1u"g/cm^3"
+                println("Expect air")
+            end
+            break
         end
-        section_length = intersection.distance - prev_distance
-        section_column_depth = section_length * density
-        if section_column_depth > target_column_depth
-            tot_distance += section_length * target_column_depth / section_column_depth
-            target_column_depth = 0.0u"g"/u"cm"^2
-        end
-        target_column_depth -= section_column_depth
-        tot_distance += section_length
-        idx += 1
+        accrued_d += dist
+        accrued_cd += segment_cd
     end
-    return tot_distance, tot_cd
+    return accrued_d, target_column_depth
+end
+
+function compute_density(
+    ixs::I,
+    d::Direction{T}
+) where {T<:Real,I<:AbstractVector{Intersection{T}}}
+
+    #prem_densities = [rock_density, rock_density, rock_density]
+    densities = Vector{Quantity{T, mdim/ldim^3}}(undef, length(ixs)-1)
+    for (idx, ix) in enumerate(ixs[1:end-1])
+        if typeof(ix)==TriangleIntersection{T}
+            entering_rock = dot(d, ix.normal) < 0
+            density = entering_rock ? rock_density : air_density
+            densities[idx] = density
+        elseif typeof(ix)==SphereIntersection{T}
+            # This is not totally try, but I think it should be okay
+            density = 2.6u"g/cm^3"
+            densities[idx] = density
+        end
+    end
+    return densities
 end

2d733a06cdf23ba5216411802fa12cccbb2020ad
jlazar17 <jeff.p.lazar@gmail.com>
2025-11-28 10:52:45 -0500

    Update weighting approach

--- src/injection/injection_event.jl
+++ src/injection/injection_event.jl
@@ -3,17 +3,13 @@ struct InjectionEvent{T <: Real}
     entry_state::Particle{T}
     initial_state::Particle{T}
     final_state::Particle{T}
-    genX::Quantity{T, mdim/ldim^2, typeof(u"g" / u"cm"^2)}
-    mc_weight::T
-    oneweight::T
+    weight_params::WeightParameters{T}
 end
 
-const null_event = InjectionEvent(
-    -1,
+null_event(id=0, initial_state=nothing) = InjectionEvent(
+    id,
+    isnothing(initial_state) ? null_particle : initial_state,
     null_particle,
     null_particle,
-    null_particle,
-    0.0*u"g" / u"cm"^2,
-    0.0,
-    0.0
+    null_params
 )
